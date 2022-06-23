fbca8a04d5e45814e0a25e4d0e8e9b94e1204759
Davner <davner23@gmail.com>
2022-06-23 03:05:19 -0400

    Continuação do jogo da velha 2 - 23/06/2022

---
+++ src/components/ScoreBoard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './ScoreBoard.css';
+
+export const ScoreBoard = ({ scores, xPlaying }) => {
+	const { xScore, oScore } = scores;
+	return (
+		<div className="scoreboard">
+			<span>X - {xScore}</span>
+			<span>O - {oScore}</span>
+		</div>
+	);
+};
--- src/components/board.js
+++ src/components/board.js
@@ -2,10 +2,13 @@ import './board.css';
 import Box from "./box";
 import React from 'react';
 
-const board = () => {
+const board = ({board, onClick}) => {
   return (
-    <div>
-      <Box value="X" onClick={null}/>
+    <div className='board'>
+      {board.map((value, idx) => {
+        return <Box value={value} onClick={() => value === null && onClick(idx)}/>
+      })}
+      
     </div>
   )
 }
